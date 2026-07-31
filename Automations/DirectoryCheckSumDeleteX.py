d1872ea0a88e9992b045e77c85d574c20162b224
Hub-Johnson <johnson.katti@gmail.com>
2026-08-01 00:07:39 +0530

    Automation

---
+++ Automations/DirectoryCheckSumDeleteX.py
@@ -0,0 +1,80 @@
+import sys
+import os
+import hashlib
+
+def CalculateCheckSum(FileName):
+    fobj = open(FileName, "rb")
+
+    hobj = hashlib.md5()
+
+    Buffer = fobj.read(1024)
+
+    while(len(Buffer) > 0):
+        hobj.update(Buffer)
+        Buffer = fobj.read(1024)
+
+    fobj.close()
+
+    return hobj.hexdigest()
+
+def FindDuplicate(DirectoryName):
+
+    Ret = False
+
+    Ret = os.path.exists(DirectoryName)
+
+    if Ret == False:
+        print("PATH is invalid")
+        return 
+
+    Ret = os.path.isdir(DirectoryName)
+
+    if Ret == False:
+        print("It is not directory")
+        return
+
+    Duplicate = {}
+
+    for FolderName, SubFolder, FileName in os.walk(DirectoryName):
+
+        for fname in FileName:
+            fname = os.path.join(FolderName, fname)
+
+            Checksum = CalculateCheckSum(fname)
+
+            # print(f"{fname} : {Checksum}")
+
+            if Checksum in Duplicate:
+                Duplicate[Checksum].append(fname)
+            else:
+                Duplicate[Checksum] = [fname]
+
+        return Duplicate
+
+def DeleteDuplicate(DirectoryName):
+    MyDict = FindDuplicate(DirectoryName)
+
+    Result = list(filter(lambda x : len(x) > 1, MyDict.values())) # very important Line
+
+    count = 0
+    TotalDeleted = 0
+
+    for value in Result:
+
+        for subvalue in value:
+            count = count + 1  # Run 4 Times
+
+            if count > 1:
+                print("Duplicate Found :", subvalue)
+                TotalDeleted = TotalDeleted + 1
+
+        count = 0
+
+    print("Total Delated Files: ", TotalDeleted)
+
+def main():
+
+    Data = DeleteDuplicate("Test")
+
+if __name__ == "__main__":
+    main()
---
+++ Automations/PlatformServillence_LogFile_Scheduler.py
@@ -0,0 +1,84 @@
+import psutil
+import sys
+import os
+import time
+import schedule
+
+def PlatforServillence(FolderName):
+    # -----------------------------------------------------
+
+    border = "_"*50
+
+    Ret = False
+
+    Ret = os.path.exists(FolderName)
+
+    if(Ret == True):
+        Ret = os.path.isdir(FolderName)
+        if(Ret == False):
+            print("Unable to processed as directory name is existing but its not a directory")
+            return
+    else:
+        os.mkdir(FolderName)
+        print("\nDirectory for the log file gets created successfully")
+
+    # -----------------------------------------------------
+    
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S")
+
+    FileName = os.path.join(FolderName, "Marvellous_%s.log"%timestamp)
+
+    fobj = open(FileName,"w")
+    print(f"\nLog file created successfully with name {FileName}")
+
+    # -----------------------------------------------------
+
+def main():
+    border = "_"*50
+
+    print(border)
+    print("\nMarvellous Platform Servillence System")
+    print(border)
+
+    # --h and --u handling
+    if(len(sys.argv) == 2):
+        if(sys.argv[1] == "--h" or sys.argv[1] == "--H"):
+            print("\nThis script use to perform")
+            print("1: It fetch the information of running process")
+            print("2: It fetch the information of running RAM")
+            print("3: It fetch the information of Secondary Stroage")
+            print("4: It fetch the information of microprocessor")
+            print("5: It get auto scheduled periodically")
+            print("6: It maintains all records into log file")
+            print("7: It send the log files through mail periodically")
+        elif(sys.argv[1] == "--u" or sys.argv[1] == "--U"):
+            print("\nUse the automation script as: ")
+            print(f"Python {sys.argv[0]} Time_Interval Folder_Name")
+            print("Time_Interval: Time in minutes for periodic execution")
+            print("Folder_Name: Name of folder for the log file creation")
+        else:
+            print("\nUnable to proceed as there is no mathching argument")
+            print("Please use --h or --u flag getting more details")
+            
+
+    # Main Business Logic
+    elif(len(sys.argv) == 3):
+        # PlatforServillence(sys.argv[2])
+
+        schedule.every(int(sys.argv[1])).minutes.do(PlatforServillence, sys.argv[2])
+
+        while True:
+            schedule.run_pending()
+            time.sleep(1) # 1 is second
+
+    else:
+        print("\nInvalid number of argument")
+        print("Unable to proceed as argument are not matching")
+        print("Please use --h or --u flag getting more details")
+
+    print(border)
+    print("\nThan you for using out Automation System")
+    print(border)
+
+if __name__ == "__main__":
+    main()
