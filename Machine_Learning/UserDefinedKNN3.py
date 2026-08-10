b233c98ada57ac4f92f07ad411fb5a34f6a5632c
Hub-Johnson <johnson.katti@gmail.com>
2026-08-10 16:03:48 +0530

    small issue fixed

--- Machine_Learning/UserDefinedKNN3.py
+++ Machine_Learning/UserDefinedKNN3.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 
 def MarvellousEucDistance(P1, P2):
-    Ans = math.sqrt((P1['X'] - P2['X']) + (P1['Y'] - P2['Y']))
+    Ans = math.sqrt((P1['X'] - P2['X'])**2 + (P1['Y'] - P2['Y'])**2)
     return Ans
 
 # User Defined KNN Classification
